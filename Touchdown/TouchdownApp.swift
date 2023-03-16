fbacbc6750b2c7f4856d6b1bb301155a3c385b05
Javier Laguna <javier.laguna@innocv.com>
2023-03-16 16:00:44 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Javier Laguna on 16/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
